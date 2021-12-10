2006d4c64a61992ae236043aafe7017bd0a7f9b7
Dom <dominic@booandjoel.co.uk>
2021-12-10 10:59:29 +0000

    Fixed param names

--- Dom.Logging/Dom.Logging.psm1
+++ Dom.Logging/Dom.Logging.psm1
@@ -1,5 +1,19 @@
 Function Write-ToLog {
 
+	<#
+        .SYNOPSIS
+            Creates a text log file and then adds content or adds content if the log file already exists
+
+        .NOTES
+            Version:        1.0.0.0
+            Author:         Dom Clayton
+
+        .EXAMPLE
+            Write-ToLog -LogFile fileName -LogContent 'Content'
+			    
+    #>
+
+
     [CmdletBinding()]
 	Param
 	(
@@ -9,17 +23,17 @@ Function Write-ToLog {
         [string]$LogContent
 	)
 	
-	If (Test-Path $sLogFile) {
+	If (Test-Path $LogFile) {
 	
 		# Append to the file
-		Add-Content -Path $sLogFile -Value $sLogContent
+		Add-Content -Path $LogFile -Value $LogContent
 		
 	}
 	Else {
 	
 		# Create the file
-		New-Item -ItemType File -Path $sLogFile
-		Add-Content -Path $sLogFile -Value $sLogContent
+		New-Item -ItemType File -Path $LogFile
+		Add-Content -Path $LogFile -Value $LogContent
 		
 	}
 }
